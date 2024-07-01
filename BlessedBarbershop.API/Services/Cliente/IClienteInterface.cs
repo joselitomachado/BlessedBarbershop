c9e1d3c951221880f196c80a834a30c5841a0937
Joselito Machado <paguelosantos@hotmail.com>
2024-07-01 09:18:30 -0300

    feat: add interfaces

---
+++ BlessedBarbershop.API/Services/Cliente/IClienteInterface.cs
@@ -0,0 +1,14 @@
+﻿using BlessedBarbershop.API.DTOs.Cliente;
+using BlessedBarbershop.API.Models;
+
+namespace BlessedBarbershop.API.Services.Cliente;
+
+public interface IClienteInterface
+{
+    Task<ResponseModel<IEnumerable<ClienteModel>>> ObterTodosClientes();
+    Task<ResponseModel<ClienteModel>> ObterClientePorNomeSobrenome(string nome, string sobrenome);
+    Task<ResponseModel<ClienteModel>> ObterClientePorID(int id);
+    Task<ResponseModel<ClienteModel>> CadastrarCliente(ClienteDto clienteDto);
+    Task<ResponseModel<ClienteModel>> AtualizarCliente(int id, ClienteDto clienteDto);
+    Task<ResponseModel<ClienteModel>> ExcluirCliente(int id);
+}
