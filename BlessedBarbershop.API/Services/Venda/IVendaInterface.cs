ca2089e7f8b95a7fbfb1a07fa579a0e62f127d9f
Joselito Machado <paguelosantos@hotmail.com>
2024-07-01 09:18:38 -0300

    feat: add interfaces

---
+++ BlessedBarbershop.API/Services/Venda/IVendaInterface.cs
@@ -0,0 +1,13 @@
+﻿using BlessedBarbershop.API.DTOs.Venda;
+using BlessedBarbershop.API.Models;
+
+namespace BlessedBarbershop.API.Services.Venda;
+
+public interface IVendaInterface
+{
+    Task<ResponseModel<IEnumerable<VendaModel>>> ObterTodasVendas();
+    Task<ResponseModel<IEnumerable<VendaModel>>> ObterVendaPorCliente(int clienteId);
+    Task<ResponseModel<IEnumerable<VendaModel>>> ObterVendaPorData(DateTime dataCadastro);
+    Task<ResponseModel<VendaModel>> CadastrarVenda(VendaDto vendaDto);
+    Task<ResponseModel<VendaModel>> ExcluirVenda(int id);
+}
