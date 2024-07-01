9332d3aeb59cf9c5b425bf1a78cacd127c349aa9
Joselito Machado <paguelosantos@hotmail.com>
2024-07-01 09:20:43 -0300

    feat: add modelo de dados

---
+++ BlessedBarbershop.API/Models/VendaModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BlessedBarbershop.API.Models;
+
+public class VendaModel
+{
+    public int Id { get; set; }
+    public int ClienteId { get; set; }
+    public ClienteModel? Cliente { get; set; }
+    public int ProdutoServicoId { get; set; }
+    public ProdutoServicoModel? ProdutoServico { get; set; }
+    public DateTime DataCadastro { get; set; }
+}
