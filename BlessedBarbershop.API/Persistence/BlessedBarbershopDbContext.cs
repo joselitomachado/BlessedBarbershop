625bb6eead5c2390c7c48f00c5d40cee79a42a91
Joselito Machado <paguelosantos@hotmail.com>
2024-07-01 09:19:10 -0300

    feat: add dbContext

---
+++ BlessedBarbershop.API/Persistence/BlessedBarbershopDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BlessedBarbershop.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlessedBarbershop.API.Persistence;
+
+public class BlessedBarbershopDbContext : DbContext
+{
+    public BlessedBarbershopDbContext(DbContextOptions<BlessedBarbershopDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<ProdutoServicoModel> ProdutosServicos { get; set; }
+    public DbSet<ClienteModel> Clientes { get; set; }
+    public DbSet<VendaModel> Vendas { get; set; }
+}
