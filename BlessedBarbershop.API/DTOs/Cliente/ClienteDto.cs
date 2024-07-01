0d8ee9a49aacc2c9eeaf3dbd2b8b78c239fa76d7
Joselito Machado <paguelosantos@hotmail.com>
2024-07-01 09:19:48 -0300

    feat: add dtos

---
+++ BlessedBarbershop.API/DTOs/Cliente/ClienteDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlessedBarbershop.API.DTOs.Cliente;
+
+public class ClienteDto
+{
+    [Required]
+    public string Nome { get; set; } = string.Empty;
+    [Required]
+    public string Sobrenome { get; set; } = string.Empty;
+    public string? Email { get; set; } = string.Empty;
+    [Required]
+    public string DDD { get; set; } = string.Empty;
+    [Required]
+    public string NumeroCelular { get; set; } = string.Empty;
+}
