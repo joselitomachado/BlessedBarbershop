ba6fe5892b1cc5d8e5991a677c66d88245ec0a96
Joselito Machado <paguelosantos@hotmail.com>
2024-07-01 09:29:14 -0300

    feat: add dbContext

--- BlessedBarbershop.API/Persistence/BlessedBarbershopDbContext.cs
+++ BlessedBarbershop.API/Persistence/BlessedBarbershopDbContext.cs
@@ -12,4 +12,19 @@ public class BlessedBarbershopDbContext : DbContext
     public DbSet<ProdutoServicoModel> ProdutosServicos { get; set; }
     public DbSet<ClienteModel> Clientes { get; set; }
     public DbSet<VendaModel> Vendas { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ProdutoServicoModel>()
+            .HasIndex(p => p.Nome)
+            .IsUnique();
+
+        modelBuilder.Entity<ClienteModel>()
+            .HasIndex(c => new { c.DDD, c.NumeroCelular })
+            .IsUnique();
+
+        modelBuilder.Entity<ClienteModel>()
+            .HasIndex(c => c.Email)
+            .IsUnique();
+    }
 }
