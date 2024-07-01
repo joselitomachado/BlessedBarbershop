bd89ca6f512dd5a025715d83536a8e12d92a3082
Joselito Machado <paguelosantos@hotmail.com>
2024-07-01 09:18:34 -0300

    feat: add interfaces

---
+++ BlessedBarbershop.API/Services/ProdutoServico/IProdutoServicoInterface.cs
@@ -0,0 +1,14 @@
+﻿using BlessedBarbershop.API.DTOs.ProdutoServico;
+using BlessedBarbershop.API.Models;
+
+namespace BlessedBarbershop.API.Services.ProdutoServico;
+
+public interface IProdutoServicoInterface
+{
+    Task<ResponseModel<IEnumerable<ProdutoServicoModel>>> ObterTodosProdutosServicos();
+    Task<ResponseModel<ProdutoServicoModel>> ObterProdutoServicoPorNome(string nome);
+    Task<ResponseModel<ProdutoServicoModel>> ObterProdutoServicoPorId(int id);
+    Task<ResponseModel<ProdutoServicoModel>> CadastrarProdutoServico(ProdutoServicoDto produtoServicoDto);
+    Task<ResponseModel<ProdutoServicoModel>> AtualizarProdutoServico(int id, ProdutoServicoDto produtoServicoDto);
+    Task<ResponseModel<ProdutoServicoModel>> ExcluirProdutoServico(int id);
+}
