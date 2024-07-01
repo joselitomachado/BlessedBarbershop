8c1610b5cc0b2cf488619cd058206969327ce304
Joselito Machado <paguelosantos@hotmail.com>
2024-07-01 09:20:49 -0300

    feat: add modelo de dados

---
+++ BlessedBarbershop.API/Models/ResponseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BlessedBarbershop.API.Models;
+
+public class ResponseModel<T>
+{
+    public T? Data { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public bool Status { get; set; } = true;
+}
