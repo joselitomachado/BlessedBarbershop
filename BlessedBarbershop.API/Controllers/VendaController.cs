1cd78636789d099ffa4c00f17221ff31e0ca2662
Joselito Machado <paguelosantos@hotmail.com>
2024-07-01 09:21:13 -0300

    feat: add controllers

---
+++ BlessedBarbershop.API/Controllers/VendaController.cs
@@ -0,0 +1,63 @@
+﻿using BlessedBarbershop.API.DTOs.Venda;
+using BlessedBarbershop.API.Models;
+using BlessedBarbershop.API.Services.Venda;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlessedBarbershop.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class VendaController : ControllerBase
+{
+    private readonly IVendaInterface _vendaInterface;
+
+    public VendaController(IVendaInterface vendaInterface)
+    {
+        _vendaInterface = vendaInterface;
+    }
+
+    [HttpGet]
+    [Route("ObterTodasVendas")]
+    public async Task<ActionResult<ResponseModel<List<VendaModel>>>> ObterTodasVendas()
+    {
+        var vendas = await _vendaInterface.ObterTodasVendas();
+
+        return Ok(vendas);
+    }
+
+    [HttpGet]
+    [Route("ObterVendaPorData/{dataCadastro}")]
+    public async Task<ActionResult<ResponseModel<VendaModel>>> ObterVendaPorData(DateTime dataCadastro)
+    {
+        var venda = await _vendaInterface.ObterVendaPorData(dataCadastro);
+
+        return Ok(venda);
+    }
+
+    [HttpGet]
+    [Route("ObterVendaPorCliente/{clienteId}")]
+    public async Task<ActionResult<ResponseModel<VendaModel>>> ObterVendaPorCliente(int clienteId)
+    {
+        var venda = await _vendaInterface.ObterVendaPorCliente(clienteId);
+
+        return Ok(venda);
+    }
+
+    [HttpPost]
+    [Route("CadastrarVenda")]
+    public async Task<ActionResult<ResponseModel<VendaModel>>> CadastrarVenda(VendaDto vendaDto)
+    {
+        var venda = await _vendaInterface.CadastrarVenda(vendaDto);
+
+        return Ok(venda);
+    }
+
+    [HttpDelete]
+    [Route("ExcluirVenda/{id}")]
+    public async Task<ActionResult<ResponseModel<VendaModel>>> ExcluirVenda(int id)
+    {
+        var venda = await _vendaInterface.ExcluirVenda(id);
+
+        return Ok(venda);
+    }
+}
