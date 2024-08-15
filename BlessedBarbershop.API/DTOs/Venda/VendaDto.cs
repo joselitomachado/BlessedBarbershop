967a259b5570a12fc1b56a4109a6046d9c3f776e
Joselito Machado <paguelosantos@hotmail.com>
2024-08-15 12:21:07 -0300

    add datanotations

--- BlessedBarbershop.API/DTOs/Venda/VendaDto.cs
+++ BlessedBarbershop.API/DTOs/Venda/VendaDto.cs
@@ -1,7 +1,11 @@
-﻿namespace BlessedBarbershop.API.DTOs.Venda;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlessedBarbershop.API.DTOs.Venda;
 
 public class VendaDto
 {
+    [Required]
     public int ClienteId { get; set; }
+    [Required]
     public int ProdutoServicoId { get; set; }
 }
